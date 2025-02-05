4abc6d20b7ce7e05c41cf1dcdb232f91f2019acd
CFaury <cedrick.Faury@ac-clermont.fr>
2025-02-05 11:51:47 +0100

    bugfix /2

--- src/Options.py
+++ src/Options.py
@@ -1140,7 +1140,7 @@ class DirSelectorCombo(wx.ComboCtrl):
         font.SetWeight(wx.FONTWEIGHT_BOLD)
         dc.SetFont(font)
         tw,th = dc.GetTextExtent(label)
-        dc.DrawText(label, (bw-tw)/2, (bw-tw)/2)
+        dc.DrawText(label, (bw-tw)//2, (bw-tw)//2)
         del dc
 
         # now apply a mask using the bgcolor
